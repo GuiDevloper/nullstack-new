aceba350738cc0d0a1a79ed973c4ebbc9c8178e0
Guilherme Correia <guicorrea384@gmail.com>
2023-07-11 10:55:54 -0300

    💄 Improve thumbnail & social card size

--- src/Application.tsx
+++ src/Application.tsx
@@ -1,14 +1,30 @@
-import Nullstack, { NullstackClientContext } from 'nullstack'
+import Nullstack, { NullstackClientContext, NullstackNode } from 'nullstack'
 
 import './Application.css'
+declare function Head(): NullstackNode
 
 class Application extends Nullstack {
 
-  prepare({ page }: NullstackClientContext) {
+  prepare({ page, project }: NullstackClientContext) {
     page.locale = 'en-US'
+    page.title = `${project.name} Full Stack Template at StackBlitz`
+    page.description = 'Run a Nullstack template with one link ✨🚀'
   }
 
-  render: () => false
+  renderHead() {
+    return (
+      <head>
+        <meta
+          name="twitter:card"
+          content="summary_large_image"
+        />
+      </head>
+    )
+  }
+
+  render() {
+    return <Head />
+  }
 
 }
 
